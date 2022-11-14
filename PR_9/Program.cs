a3abac7ef3ac86009c2f68f269b82ba830643ddc
StanislavK2004 <117818323+StanislavK2004@users.noreply.github.com>
2022-11-14 11:54:27 +0300

    Add files via upload

---
+++ PR_9/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PR_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Практическая работа №9";
+            Console.WriteLine("Здравствуйте!");
+
+            const int m = 15; // задание размерности массива
+            int[] A = new int[m]; // объявление одномерного массива
+            int i = 0;
+            bool err; // err - флаг обнаружения ошибки при вводе элементов массива
+
+            while (i < m)
+            {
+                err = false;
+                Console.WriteLine("Введите" + i + "элемент массива");
+                try
+                {
+                    A[i] = Convert.ToInt32(Console.ReadLine());
+                }
+                catch (FormatException e) // обработка исключения связанного с фоматом
+                {
+                    err = true;
+                    Console.WriteLine("Возникла ошибка:" + e.Message);
+                }
+                if (!err) i++;
+            }
+            for(i=0;i<m; i++)
+            {
+               R
+            }
+                
+    }
+}
